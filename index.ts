c3d9c55fbc35db523060dba53616636ab141ea34
Soumya Ranjan Das <soumyaranjand295@gmail.com>
2025-09-17 08:21:54 +0530

    feat: Implement interactive CLI with language model integration
    - Added ChatGroq for LLM invocation with customizable parameters.
    - Created a state graph to manage conversation flow.
    - Implemented callLLM function to handle user messages and return assistant responses.
    - Enhanced main function to support user input and display assistant replies.
    - Updated package.json to include necessary LangChain dependencies.

--- index.ts
+++ index.ts
@@ -1,6 +1,36 @@
+import { MessagesAnnotation, StateGraph } from '@langchain/langgraph';
 import readline from 'node:readline/promises';
+import { ChatGroq } from '@langchain/groq';
+/* 
+1. Create Node Functions.
+2. Build the graph.
+3. Compile and Execute the graph.
+*/
+
+const chat = new ChatGroq({
+  model: "openai/gpt-oss-120b",
+  maxRetries: 2,
+  temperature: 0.7,
+});
+
+async function callLLM(state: { messages: Array<{ role: string; content: string }> }): Promise<Object> {
+  // Simulate a call to a language model
+  // console.log("Calling LLM with state:", state);
+const response = await chat.invoke(state.messages);
+// console.log("ASSISTANT:", response.text);
+// console.log("response", response);
+// console.log("response text", response.text);
+
+  return { messages: [...state.messages, { role: "assistant", content: response.content }] };
+}
+
+/* Build the graph */
+
+const graph = new StateGraph(MessagesAnnotation);
+graph.addNode("agent",callLLM).addEdge("__start__","agent").addEdge("agent","__end__");
 
 
+const app = graph.compile();
 
 async function main() {
   console.log("Welcome to the Interactive CLI Application!");
@@ -14,19 +44,23 @@ async function main() {
 
     const userInput = await rl.question("USER: ");
 
-    if(userInput === '/bye') break;
+    if(userInput === '/bye'){
+      rl.close();
+      break;
+    };
 
     if(userInput === '/clear'){
       console.clear();
       continue;
     }
+    const finalstate:any=await app.invoke({
+      messages: [{ role: "user", content: userInput }]
+    });
 
-    console.log(`You said ${userInput}`)
-  
+    console.log("ASSISTANT:",  finalstate.messages[finalstate.messages.length - 1].content);
   }
   rl.close();
   
 }
-
 
 main();
